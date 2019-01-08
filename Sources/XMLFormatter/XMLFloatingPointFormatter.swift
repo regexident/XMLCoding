ba9b7c8ab8e9ef9ad7becca250bdbb9ae24f456a
Vincent Esche <regexident@gmail.com>
2019-01-08 12:24:36 +0100

    Added implementation of `XMLFloatingPointFormatter`

---
+++ Sources/XMLFormatter/XMLFloatingPointFormatter.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+public protocol FixedWidthFloatingPoint: Comparable, ExpressibleByFloatLiteral {
+    var isNaN: Bool { get }
+    var isInfinite: Bool { get }
+    
+    init?<S>(_ text: S) where S : StringProtocol
+}
+
+extension Float: FixedWidthFloatingPoint {
+    // Required methods are already provided by Float
+}
+
+extension Double: FixedWidthFloatingPoint {
+    // Required methods are already provided by Float
+}
+
+public struct XMLFloatingPointFormatter<T: FixedWidthFloatingPoint> {
+    public typealias Value = T
+    
+    public enum Error: Swift.Error {
+        case invalidValue
+    }
+}
+
+extension XMLFloatingPointFormatter: XMLFormatter {
+    public func value(from string: String) throws -> Value {
+        guard let value = Value(string) else {
+            throw Error.invalidValue
+        }
+        return value
+    }
+    
+    public func string(from value: Value) throws -> String {
+        guard !value.isNaN else {
+            return "NaN"
+        }
+        
+        guard !value.isInfinite else {
+            return (value > 0.0) ? "INF" : "-INF"
+        }
+        
+        return "\(value)"
+    }
+}
+
+public typealias XMLFloatFormatter = XMLFloatingPointFormatter<Float>
+public typealias XMLDoubleFormatter = XMLFloatingPointFormatter<Double>
+
+public typealias XMLFloat32Formatter = XMLFloatFormatter
+public typealias XMLFloat64Formatter = XMLDoubleFormatter
---
+++ Tests/XMLFormatterTests/XMLFloatingPointFormatterTests/XMLFloatFormatterTests.swift
@@ -0,0 +1,73 @@
+import XCTest
+@testable import XMLFormatter
+
+class XMLFloatFormatterTests: XCTestCase {
+    typealias Formatter = XMLFloatFormatter
+    typealias Value = Formatter.Value
+    
+    func test_string_from_value() throws {
+        let examples: [(Value, String)] = [
+            (-3e2, "-300.0"),
+            (4268.22752e11, "4.2682274e+14"),
+            (+24.3e-3, "0.0243"),
+            (12.3, "12.3"),
+            (-12.3, "-12.3"),
+            (0.0, "0.0"),
+            (-0.0, "-0.0"),
+            (.infinity, "INF"),
+            (-.infinity, "-INF"),
+            (.nan, "NaN"),
+        ]
+        
+        let formatter = Formatter()
+        
+        for (value, expected) in examples {
+            let string = try formatter.string(from: value)
+            XCTAssertEqual(string, expected)
+        }
+    }
+    
+    func test_value_from_string_valid() throws {
+        let examples: [(String, Value)] = [
+            ("-3E2", -3E2),
+            ("4268.22752E11", 4268.22752E11),
+            ("+24.3e-3", +24.3e-3),
+            ("12", 12.0),
+            ("+3.5", +3.5),
+            ("INF", .infinity),
+            ("-INF", -.infinity),
+            ("0", 0.0),
+            ("-0", -0.0),
+            ("NaN", .nan),
+        ]
+
+        let formatter = Formatter()
+
+        for (string, expected) in examples {
+            let value = try formatter.value(from: string)
+            
+            if expected.isNaN {
+                XCTAssertTrue(value.isNaN)
+            } else {
+                XCTAssertEqual(value, expected)
+            }
+        }
+    }
+
+    func test_value_from_string_invalid() throws {
+        let examples: [String] = [
+            "-3E2.4",
+            "12E",
+            "  42  ",
+            "foobar",
+            "      ",
+            "",
+        ]
+
+        let formatter = Formatter()
+
+        for string in examples {
+            XCTAssertThrowsError(try formatter.value(from: string))
+        }
+    }
+}
