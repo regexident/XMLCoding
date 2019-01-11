57084fe9d17777c926cfd3bad61437bdaacde8ba
Vincent Esche <regexident@gmail.com>
2019-01-11 11:28:19 +0100

    Added `func data(from:)` method & default implementation to `XMLFormatter`

--- Sources/XMLFormatter/XMLFormatter.swift
+++ Sources/XMLFormatter/XMLFormatter.swift
@@ -1,8 +1,26 @@
 import Foundation
 
+public enum XMLFormatterError: Error {
+    case invalidStringEncoding
+}
+
 public protocol XMLFormatter {
     associatedtype Value
-    
+
     func value(from string: String) throws -> Value
     func string(from value: Value) throws -> String
+
+    func data(from value: Value) throws -> Data
+}
+
+extension XMLFormatter {
+    public func data(from value: Value) throws -> Data {
+        let string = try self.string(from: value)
+
+        guard let data = string.data(using: .utf8) else {
+            throw XMLFormatterError.invalidStringEncoding
+        }
+
+        return data
+    }
 }
