e7433bc179452caf1235cac61579777a3d9f230c
Vincent Esche <regexident@gmail.com>
2019-11-13 12:13:50 +0100

    Cleaned up ‘Package.swift’ file

--- Package.swift
+++ Package.swift
@@ -6,74 +6,100 @@ import PackageDescription
 let package = Package(
     name: "XMLCoding",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "XMLDocument",
-            targets: ["XMLDocument"]
+            targets: [
+                "XMLDocument",
+            ]
         ),
         .library(
             name: "XMLFormatter",
-            targets: ["XMLFormatter"]
+            targets: [
+                "XMLFormatter",
+            ]
         ),
         .library(
             name: "XMLReader",
-            targets: ["XMLReader"]
+            targets: [
+                "XMLReader",
+            ]
         ),
         .library(
             name: "XMLWriter",
-            targets: ["XMLWriter"]
+            targets: [
+                "XMLWriter",
+            ]
         ),
         .library(
             name: "XMLCoding",
-            targets: ["XMLCoding"]
+            targets: [
+                "XMLCoding",
+            ]
         ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "XMLDocument",
-            dependencies: []
+            dependencies: [
+            ]
         ),
         .target(
             name: "XMLFormatter",
-            dependencies: []
+            dependencies: [
+            ]
         ),
         .target(
             name: "XMLReader",
-            dependencies: ["XMLDocument"]
+            dependencies: [
+                "XMLDocument",
+            ]
         ),
         .target(
             name: "XMLWriter",
-            dependencies: ["XMLDocument"]
+            dependencies: [
+                "XMLDocument",
+            ]
         ),
         .target(
             name: "XMLCoding",
-            dependencies: ["XMLDocument", "XMLReader", "XMLWriter", "XMLFormatter"]
+            dependencies: [
+                "XMLDocument",
+                "XMLReader",
+                "XMLWriter",
+                "XMLFormatter",
+            ]
         ),
         .testTarget(
             name: "XMLDocumentTests",
-            dependencies: ["XMLDocument"]
+            dependencies: [
+                "XMLDocument",
+            ]
         ),
         .testTarget(
             name: "XMLFormatterTests",
-            dependencies: ["XMLFormatter"]
+            dependencies: [
+                "XMLFormatter",
+            ]
         ),
         .testTarget(
             name: "XMLReaderTests",
-            dependencies: ["XMLReader"]
+            dependencies: [
+                "XMLReader",
+            ]
         ),
         .testTarget(
             name: "XMLWriterTests",
-            dependencies: ["XMLWriter"]
+            dependencies: [
+                "XMLWriter",
+            ]
         ),
         .testTarget(
             name: "XMLCodingTests",
-            dependencies: ["XMLCoding"]
+            dependencies: [
+                "XMLCoding",
+            ]
         ),
     ]
 )
